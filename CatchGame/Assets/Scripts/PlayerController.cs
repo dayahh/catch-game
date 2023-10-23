758e7ccca4a226f8438c6b456390e0cef408b2bc
Adaya Head <headadaya@gmail.com>
2023-10-23 17:18:08 -0400

    Refactor player move
    Move ability is now its own method

--- CatchGame/Assets/Scripts/PlayerController.cs
+++ CatchGame/Assets/Scripts/PlayerController.cs
@@ -14,9 +14,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        mouseMove();
+    }
+
+    //moves the player character around
+    void mouseMove(){
         Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         position.z = 0;
         position.y = -4;
         transform.position = position;
     }
+
 }
