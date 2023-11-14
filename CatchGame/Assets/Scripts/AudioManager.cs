6e6fb618cb202632b27d8bded6fac48bcf0e51ee
Adaya Head <headadaya@gmail.com>
2023-11-14 03:09:08 -0500

    Bg music added

---
+++ CatchGame/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSpurce;
+
+    public AudioClip background;
+    public AudioClip glitchPaintSplat;
+
+
+    private void Awake(){
+        GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
+        if(musicObj.Length > 1){
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
